97ea8ded954567541b49559b82ca5149cc339873
ArthurV10 <arthurvms10@gmail.com>
2024-08-15 00:35:30 -0300

    feat: added utilities in the python code and updated WorldPlay

---
+++ Códigos Python/WorldPlay - Jogo de Palavras/utilities.py
@@ -0,0 +1,68 @@
+def pressEnter():
+    input("Press Enter To Continue")
+
+def loadArchive(archive):
+    file = open(archive)
+    arrayArchive = []
+    for phrase in file:
+        print("Arquivo sendo carregado: ", phrase)
+        arrayArchive = arrayArchive + [phrase]
+    return arrayArchive
+
+def verificationNumber(number, MinNumber, MaxNumber):
+    num = number
+    while number > MaxNumber or number < MinNumber:
+        print("Digitação Inválida!")
+        num = int(input("Digite novamente: "))
+    return num
+
+def abcderian(line):
+    valorAsciiAnterior = ord(line[0])
+    for letter in line[1:]:  # Começa a partir da segunda letra
+        if ord(letter) < valorAsciiAnterior:  # Se a letra atual for menor, a ordem está errada
+            return False
+        valorAsciiAnterior = ord(letter)  # Atualiza o valor anterior para a letra atual
+    return True
+
+def customSplit(phrase):
+    prohibitedList = []
+    for letter in phrase:
+        prohibitedList = prohibitedList + [letter]
+    return prohibitedList
+
+def customLen(phrase):
+    count = -1
+    for letter in phrase:
+        count += 1
+    return count
+
+def allLettersContain(phrase, VectorLetters):
+    for letter in VectorLetters:
+        if letter not in phrase:
+            return False
+    return True
+
+def wordsContain(word, vectorLetters):
+    for letter in word:
+        if letter in vectorLetters:
+            return True
+    return False
+
+def hasE(char):
+    return "E" in char or "e" in char
+
+def clearScreen():
+    import os
+    import platform
+
+    # Verifica qual sistema operacional está sendo utilizado
+    if platform.system() == "Windows":
+        os.system("cls")  # Comando para Windows
+    else:
+        os.system("clear")  # Comando para Linux e macOS
+
+def calculatePercentage(partialValue, totalValue):
+    if totalValue == 0:
+        return 0
+    percent = (partialValue / totalValue) * 100
+    return percent
--- Códigos Python/WorldPlay - Jogo de Palavras/worldPlay.py
+++ Códigos Python/WorldPlay - Jogo de Palavras/worldPlay.py
@@ -1,50 +1,157 @@
+from utilities import *
 def showMenu():
     print("""
     ==========|Menu|===========
-    01  - Ainda não tem nada
-    02  - Palavras com 20 letras
-    03  - 
-    10 - Encerrar
+    01 - Carregar Arquivo
+    02 - Palavras com 20 letras
+    03 - Palavras sem "E"
+    04 - Letras Proibidas
+    05 - Letras Permitadas
+    06 - Letras Obrigatorias
+    07 - Ordem Alfabetica
+    08 - Encerrar
     ===========================""")
-
-def clearScreen():
-    import os
-    os.system("cls")
-
-def countChar(word):
-    count = 0
-    for char in word:
-        count += 1
-    return count
 
 
 def print20Char(archive):
+    count = 0
     for line in archive:
-        if countChar(line) == 20:
-            print(line)
+        if customLen(line) >= 20:
+            count += 1
+            print(f"""
+            Palavra {count}:
+            ==> {line}
+            Tamanho da Palavra: {customLen(line)}
+            """)
 
-def pressEnter():
-    press = (input("Press Enter To Continue"))
-    return
+def hasNoEQuestion(archive):
+    count = 0
+    countTotal = 0
+    for line in archive:
+        countTotal += 1
+        if not hasE(line):  # Se não contém "E" ou "e"
+            count += 1
+            print(f"""
+            Palavra {count}:
+            ==> {line}
+            """)
+    print(f"Palavras sem 'E': {calculatePercentage(count, countTotal):.2f}%")
+
+def prohibitedLetter(archive, phrase):
+    count = 0
+    countTotal = 0
+    prohibitedList = customSplit(phrase)
+    for word in archive:
+        countTotal+= 1
+        if not wordsContain(word, prohibitedList):
+            count +=1
+            print(f"""
+            Palavra {count}
+            ==> {word}
+            """) 
+    print("Quantidade de Palavras Proibidas: ", countTotal - count)
+
+def permittedLetters(archive, phrase):
+    count = 0
+    permittedList = customSplit(phrase)
+    for words in archive:
+        if wordsContain(words, permittedList):
+            count += 1
+            print(f"""
+            Palavra {count}
+            ==> { words}
+            """)
+
+def mandatoryLetters(archive, phrase):
+    count = 0
+    for words in archive:
+        if allLettersContain(words, phrase):
+            count += 1
+            print(f"""
+            Palavra {count}
+            ==> {words}
+            """)
+
+def printSequenceAbcdarian(archive):
+    count = 0
+    total_palavras = 0
+    for line in archive:
+        total_palavras += 1
+        line = line.strip()  # Removendo espaços e quebras de linha
+        if abcderian(line):  # Verifica se a palavra está em ordem alfabética
+            count += 1
+            print(f"""
+            Palavra {count} em ordem alfabética:
+            ==> {line}
+            """)  
 
 def main():
-    ##TODO Ajeitar, remover espaços em branco
-    ## Arquivo nescessário para a questão
-    file = open("words.txt")
-    ## Variável para escolha
-    opção = 0
-    ## limpar tela, apresentar menu e permite escolha de item no menu
-    while opção != 10:
+    option = 0
+    file = ""
+    while option != 8:
+        file = open("words.txt")
         clearScreen()
         showMenu()
-        opção = int(input(">>> "))
-        ## Dependendo da escolha, alguma ação ocorrerá! 
-        if opção == 2:
-            print20Char(file)
+        option = verificationNumber(int(input(">>> ")), 0, 8)
+
+        if option == 1:
+            file = loadArchive("words.txt")
+            print("Arquivo Carregado!")
             pressEnter()
+
+        if option == 2:
+            if file == "":
+                clearScreen()
+                print("Arquivo Ainda Não Carregado!")
+                pressEnter()
+            elif file != "":
+                print20Char(file)
+                pressEnter()
+
+        elif option == 3:
+            if file == "":
+                clearScreen()
+                print("Arquivo Ainda Não Carregado!")
+                pressEnter()
+            elif file != "":
+                hasNoEQuestion(file)
+                pressEnter()
+
+        elif option == 4:
+            if file == "":
+                clearScreen()
+                print("Arquivo Ainda Não Carregado!")
+                pressEnter()
+            elif file != "":
+                prohibitedLetter(file, input("Digite as letras proibidas: "))
+                pressEnter()
+
+        elif option == 5:
+            if file == "":
+                clearScreen()
+                print("Arquivo Ainda Não Carregado!")
+                pressEnter()
+            elif file != "":
+                permittedLetters(file, input("Digite as letras permitidas: "))
+                pressEnter()
+        
+        elif option == 6:
+            if file == "":
+                clearScreen()
+                print("Arquivo Ainda Não Carregado!")
+                pressEnter()
+            elif file != "":
+                mandatoryLetters(file, input("Digite as letras obrigatorias: "))
+                pressEnter()
+        
+        elif option == 7:
+            if file == "":
+                clearScreen()
+                print("Arquivo Ainda Não Carregado!")
+                pressEnter()
+            elif file != "":
+                printSequenceAbcdarian(file)
+                pressEnter()
+    print("Encerrando Programa...")
+    
 main()
-
-
-
-
-
